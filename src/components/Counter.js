26f17ccd49def1a16939e95edc85c3cb9817f48a
chandrathemerciless <chandra.asar@gmail.com>
2024-06-07 14:59:05 -0400

    Adding another state prop to the global state and a payload to the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,41 @@
 import React from'react';
-import { useSelector } from'react-redux';
+import { useSelector, useDispatch } from'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector( state => state.counter );
+  const showCounter = useSelector( state => state.showCounter );
 
-  const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+  }
+
+  const increaseHandler = () => {
+    dispatch({ type: 'INCREASE', amount: 5 });
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE_COUNTER' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      
+      {showCounter && 
+        <div>
+          <button onClick={incrementHandler}>Increment</button>
+          <button onClick={increaseHandler}>Increase by five</button>
+          <button onClick={decrementHandler}>Decrement</button>
+        </div>
+      }
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import {createStore} from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) => {
+const counterReducer = (state = {counter: 0, showCounter: true}, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {...state, counter: state.counter + 1};
+        case 'INCREASE':
+            return {...state, counter: state.counter + action.amount};
         case 'DECREMENT':
             return {...state, counter: state.counter - 1};
+        case 'TOGGLE_COUNTER':
+            return {...state, showCounter: !state.showCounter};
         default:
             return state;
     }
