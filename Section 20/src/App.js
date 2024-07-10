efdc62b479e9127869aac513c399e822bb30a629
chandrathemerciless <chandra.asar@gmail.com>
2024-07-10 15:33:13 -0400

    Created a firebase test backend and added a fetch request for cart items - currently, reloading the app will wipe out what is stored in firebase, so upcoming commits will fix that

--- Section 20/src/App.js
+++ Section 20/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from'react';
+
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +8,14 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.showCart.showCart);
+  const cartItems = useSelector((state) => state.cartItems.cartItems);
+
+  useEffect(() => {
+    fetch('https://react-redux-dummy-440bc-default-rtdb.firebaseio.com/.json', {
+      method: 'PUT',
+      body: JSON.stringify(cartItems),
+    })
+  }, [cartItems])
 
   return (    
     <Layout>
