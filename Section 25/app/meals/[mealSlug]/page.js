6b0562074a4a356ae6158d91fad41eaf680152ad
chandrathemerciless <chandra.asar@gmail.com>
2024-08-19 15:32:30 -0400

    more progress on showing individual mealSlug content

--- Section 25/app/meals/[mealSlug]/page.js
+++ Section 25/app/meals/[mealSlug]/page.js
@@ -1,9 +1,33 @@
-import classes from './page.module.css';
+import Image from 'next/image';
 
-export default function MealDetailsPage() {
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({ params }) {
+  const {title, image, creator_email, creator, summary, instructions} = getMeal(params.mealSlug);
+
+  const formattedInstructions = instructions.replace(/\n/g, '<br/>');
+
   return (
     <>
-      
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto: ${creator_email}`}>{creator}</a>
+          </p>
+          <p className={classes.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        {/* if dangerouslySetInnerHTML is not validated, you open yourself up to XXS attacks */}
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: formattedInstructions
+        }}></p>
+      </main>
     </>
   );
 }
--- Section 25/lib/meals.js
+++ Section 25/lib/meals.js
@@ -6,6 +6,14 @@ export async function getMeals() {
     // adding arbitrary delay to simulate slower network and need for promise in main server component. Also, to explore loading states
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
+    // throw new Error("Failed to fetch meals");
+    
+
     // all() means get all rows in db
     return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug) {
+    // under the hood, using the "get" method like this is protected from sql injections
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
